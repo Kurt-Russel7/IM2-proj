356b108f081125b21dd136e986062e72f55b4b8a
Kurt-Russel7 <kurtrusseljimenez7@gmail.com>
2025-09-03 19:38:56 +0800

    v1

--- MTable.py
+++ MTable.py
@@ -1,16 +1,20 @@
-row = int (input("Enter number of rows: "))
+while (True):
 
-col = int (input("Enter number of columns: "))
+    row = int (input("Enter number of rows: "))
+    if row == 0:
+        break;
+    col = int (input("Enter number of columns: "))
+    if col == 0:
+        break;
+    search = int (input("Search a number: "))
 
-search = int (input("Search a number: "))
 
-
-for row in range (1, row + 1):
-    for col in range (1, col + 1):
-        val = row * col
-        if val == search:
-            print(f"[{val:^3}]", end = " ")
-        else:
-            print (f"{val:5}", end = " ")
-    print()
+    for rows in range (1, row + 1):
+        for cols in range (1, col + 1):
+            val = row * col
+            if val == search:
+                print(f"[{val:^3}]", end = " ")
+            else:
+                print (f"{val:5}", end = " ")
+        print()
         
