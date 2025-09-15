b16d70935b57265613b4d2b7905582bbba07753c
Kurt-Russel7 <kurtrusseljimenez7@gmail.com>
2025-09-15 22:32:59 +0800

    master

--- Dictionary.py
+++ Dictionary.py
@@ -1,16 +1,13 @@
-
 mydict = {
 
     }
 
-
-    
-    
 matrix = int (input("Matrix Size: "))
 for x in range (matrix):
-    val = input(f"Shopping items {x + 1}")
+    val = input(f"Shopping items {x + 1}: ")
     mydict [x] = val
         
+
 
     print (mydict)
 while(True):
@@ -18,15 +15,45 @@ while(True):
     
     match opt:
         case 'C':
-            print ("change")
+            changeKey = int (input("Enter key to change value:"))
+            if changeKey in mydict:
+                print (f"Found '{mydict.get(changeKey)}'  item")
+                changeVal = input("Enter Value:")
+                mydict[changeKey] = changeVal
+            else:
+                 print("I'm sorry, not in the cart")
+
+
+
         case 'R':
-            print ("Remove")
+            DelKey = int (input("Enter a key you want to delete:"))
+            if DelKey in mydict:
+                mydict.pop(DelKey)
+            else:
+                Delval = input("Enter item you want to delete:")
+                if Delval in mydict.values():
+                    mydict.popitem()
+                else:
+                    print("Item not found")
+
         case 'D':
             print (mydict)
         case 'S':
-            findKey = int (input("Enter a key you want to search:"))
-            if findKey in mydict:
-                print(mydict.get(findKey))
+            findval = input("Enter item to search:")
+            if findval in mydict.values():
+                print (f"Found '{findval}'  item")
+
             else:
-             print("I'm sorry, not in the cart")
+
+
+                findKey = int (input("Enter a key you want to search:"))
+                if findKey in mydict:
+                    print(mydict.get(findKey))
+                else:
+                 print("I'm sorry, not in the cart")
+
+        case '*':
+            print ("bye")
+            break
+
         
